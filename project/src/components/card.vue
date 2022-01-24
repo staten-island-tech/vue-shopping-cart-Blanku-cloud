41ca8bc63839471ca5a33fbb2b03d3e0dfee93d2
Blanku-cloud <pokemondinner321@gmail.com>
2022-01-24 10:14:12 -0500

    I did work

---
+++ project/src/components/card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <h2 class="item">Mew</h2>
+    <img
+      src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
+      alt="picture of mew"
+    />
+    <h3 class="price">Priceless</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
+
+<style scoped>
+html,
+body,
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.card {
+  border: 1rem solid black;
+}
+.price,
+.item {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card></Card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Card from "../components/card.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+};
 </script>
